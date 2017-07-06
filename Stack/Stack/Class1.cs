ef80b5827c832555261837095f66de6fe608a1bd
mrapp17 <mrapp17@vt.edu>
2017-07-05 21:10:10 -0500

    Stack Implementation and Unit Tests

---
+++ Stack/Stack/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private List<T> dataList;
+
+        //Constructor creates empty stack
+        public Stack()
+        {
+            dataList = new List<T>();
+        }
+
+        //Constructor creates a stack and puts the input parameter onto the stack
+        public Stack(T input)
+        {
+            dataList = new List<T>();
+            dataList.Add(input);
+        }
+
+        //Push adds an element to the top of the stack
+        public void Push(T input)
+        {
+            dataList.Add(input);
+        }
+
+        //Size returns the number of elements currently in the stack
+        public int Size()
+        {
+            return dataList.Count;
+        }
+        //PeakTop looks at the first element on the stack
+        public T Peak()
+        {
+            return dataList[dataList.Count -1];
+        }
+
+        //PopLook removes the top element and returns it
+        public T PopLook()
+        {
+            T temp = dataList[dataList.Count - 1];
+            dataList.RemoveAt(dataList.Count - 1);
+            return temp;
+        }
+
+        //Pop remove the top element but does not return it
+        public void Pop()
+        {
+            dataList.RemoveAt(dataList.Count - 1);
+        }
+    }
+}
---
+++ Stack/StackTests/StackTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stack.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        [TestMethod()]
+        public void PushTest()
+        {
+            Stack<int> testStack = new Stack<int>();
+            for(int i = 0; i < 10; ++i)
+            {
+                testStack.Push(i);
+            }
+            for(int i = 9; i > 0; --i)
+            {
+                Assert.AreEqual(i, testStack.PopLook());
+            }
+        }
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            Stack<int> testStack = new Stack<int>();
+            for(int i = 0; i < 25; ++i)
+            {
+                testStack.Push(i);
+                Assert.AreEqual(i + 1, testStack.Size());
+            }
+            for (int i = 24; i > 0; --i)
+            {
+                testStack.Pop();
+                Assert.AreEqual(i, testStack.Size());
+            }
+        }
+
+        [TestMethod()]
+        public void PeakTest()
+        {
+            Stack<int> testStack = new Stack<int>();
+            for (int i = 0; i < 25; ++i)
+            {
+                testStack.Push(i);
+                Assert.AreEqual(i, testStack.Peak());
+            }
+        }
+
+        [TestMethod()]
+        public void PopLookTest()
+        {
+            Stack<int> testStack = new Stack<int>();
+            for (int i = 0; i < 25; ++i)
+            {
+                testStack.Push(i);
+            }
+            for (int i = 24; i > 0; --i)
+            {
+                Assert.AreEqual(i, testStack.PopLook());
+            }
+        }
+
+        [TestMethod()]
+        public void PopTest()
+        {
+            Stack<int> testStack = new Stack<int>();
+            for (int i = 0; i < 25; ++i)
+            {
+                testStack.Push(i);
+            }
+            for (int i = 24; i >= 0; --i)
+            {
+                testStack.Pop();
+                Assert.AreEqual(i, testStack.Size());
+            }
+            Assert.AreEqual(0, testStack.Size());
+        }
+    }
+}
